efcc8c9d399566b8ca43370f8f7f40305fd2f74c
valery <valery.fremaux@gmail.com>
2016-05-04 12:36:21 +0200

    resync vflibs with modified TCPDF

--- version.php
+++ version.php
@@ -1,8 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die;
+/**
+ * @package local_vflibs
+ * @author Valery Fremaux valery.fremaux@gmail.com
+ */
 
-$plugin->version  = 2015101800;   // The (date) version of this plugin
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2016040300;   // The (date) version of this plugin
 $plugin->requires = 2011120500;   // Requires this Moodle version
-$plugin->release = '2.7.0 (Build 2015101800)';   // Release
+$plugin->release = '2.8.0 (Build 2016040300)';   // Release
 $plugin->component = 'local_vflibs';

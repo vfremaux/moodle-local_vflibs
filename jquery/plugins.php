3418d25babf2d1ec96041eb44b638b7929b79344
valery <valery.fremaux@gmail.com>
2016-10-30 22:56:29 +0100

    Automated sync from SVN

--- jquery/plugins.php
+++ jquery/plugins.php
@@ -1,4 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_vflibs
+ * @author valery.fremaux@gmail.com
+ * @category local
+ */
+defined('MOODLE_INTERNAL') || die;
 
 $plugins = array(
         'sparklines' => array('files' => array('sparklines/sparklines.min.js')),
@@ -55,6 +76,15 @@ $plugins = array(
         'jqwidgets-progressbar' => array(
             'files' => array(
                 'jqwidgets/jqwidgets/jqxprogressbar.js'
+            )
+        ),
+        'jqwidgets-bulletchart' => array(
+            'files' => array(
+                'jqwidgets/jqwidgets/jqxcore.js',
+                'jqwidgets/jqwidgets/jqxdata.js',
+                'jqwidgets/jqwidgets/jqxtooltip.js',
+                'jqwidgets/jqwidgets/jqxbulletchart.js',
+                'jqwidgets/jqwidgets/styles/jqx.base.css'
             )
         )
 );

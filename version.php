7d96f3da5642d3e677e0ed4d2f2d5158ad593a67
valery <valery.fremaux@gmail.com>
2019-03-21 11:45:26 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2018021005;   // The (date) version of this plugin.
-$plugin->requires = 2018042700;   // Requires this Moodle version.
-$plugin->release = '3.5.0 (Build 2018021005)';   // Release.
+$plugin->requires = 2018112800;   // Requires this Moodle version.
+$plugin->release = '3.6.0 (Build 2018021005)';   // Release.
 $plugin->component = 'local_vflibs';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.5.0002';
+$plugin->codeincrement = '3.6.0002';
